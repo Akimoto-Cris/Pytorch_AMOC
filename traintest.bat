15959895d2570f5425f4754ed40a729da9368704
Akimoto-Cris <akimotohisashibi@outlook.com>
2019-11-22 13:41:01 +0800

    reconstruct the project

--- traintest.bat
+++ traintest.bat
@@ -1,5 +1,5 @@
 @echo off
 call conda activate torch
 cd /d D:\programs\PyTorch_AMOC
-call python videoReid.py --usePredefinedSplit -l 1e-3 --saveFileName amoc_orth_clip -p trainedNets\amoc_orth_clip_model_799.pth
+call python videoReid.py --usePredefinedSplit -l 1e-3 --saveFileName amoc_orth_clip -p trainedNets\amoc_orth_clip_model_802.pth
 
